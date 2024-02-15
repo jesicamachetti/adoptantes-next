984951e1bde0cc34daf126c478683931e4628a64
Jesica <jesicamachetti@Air-de-Jesica.fibertel.com.ar>
2024-02-15 00:27:08 -0300

    edite footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,50 +1,58 @@
 import Link from "next/link";
 import Image from "next/image";
+import style from "../styles/footer.module.css"
 
 import logo from "./../public/images/Group 3.png";
 
 export default function Footer() {
   return (
-    <footer>
-      <div>
-        <Image src={logo} alt="" />
-        <nav>
-          <div>
-            <h6>Explorar</h6>
-            <div>
+    <footer className={style.footer}>
+      <div className={style.logo}>
+        <Image  src={logo} alt="" /> </div>
+        <nav className={style.navegacion}>
+          <div className={style.contenedor}>
+            <h6 className={style.h6}>Explorar</h6>
+            <div className={style.columna}>
               <Link href="/inicio">Inicio</Link>
               <Link href="/sobreNosotros">Sobre Nosotros</Link>
               <Link href="/adoptame">Adoptame</Link>
               <Link href="/contactanos">Contactanos</Link>
             </div>
           </div>
-          <div>
-            <h6>Links</h6>
-            <div>
+          <div className={style.contenedor}>
+            <h6 className={style.h6}>Links</h6>
+            <div className={style.columna}>
               <Link href="/inicio">FAQs</Link>
               <Link href="/ayuda">Ayuda</Link>
               <Link href="/politicaDePrivacidad">Politica de privacidad</Link>
               <Link href="/terminosYCondiciones">Terminos y condiciones</Link>
             </div>
           </div>
-          <div>
-            <h6>Contacto</h6>
+          <div className={style.contenedor}>
+            <h6 className={style.h6}>Contacto</h6>
+            <p> Avenida Siempreviva 742, Bs. As.</p>
+            <p> @patitasfelices@gmail.com</p>
+            <p> (+52)11 6151-3562</p>
+            
+
           </div>
-          <div>
-            <h6>Seguinos en nuestras Redes</h6>
+          <div className={style.contenedor}>
+            <h6 className={style.h6}>Seguinos en nuestras Redes</h6>
             <div>
               {/* <Image />
               <Image />
               <Image /> */}
             </div>
+            <div>
             <input type="text" />
-            <button>Subscribirse</button>
+            <button className={style.boton}>Subscribirse</button>
+            </div>
           </div>
         </nav>
-      </div>
+      
       <div>
         {/* <Image /> */}
-        <p>Todos los derechos reservados</p>
+        <p className={style.parrafo}>Todos los derechos reservados</p>
       </div>
     </footer>
   );
